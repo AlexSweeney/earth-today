b53fe300ec276548ecf867357788734486452ee6
Alex Sweeney <alexsweeney@live.co.uk>
2022-01-04 13:57:22 +0700

    fix - show result number

--- exercise-02b.js
+++ exercise-02b.js
@@ -3,21 +3,21 @@ const promApi = new Promise(function(res, rej) {
   res()
 });
 
-function logResult(result) {
-  console.log('result: ', result)
+function logResult(resultNum, result) {
+  console.log(`result${resultNum}: `, result)
 }
 
-promApi.then(logResult('1'))
-.then(logResult('2'))
-.then(logResult('3'))
+promApi.then(logResult('', '1'))
+.then(logResult('2', '2'))
+.then(logResult('3', '3'))
 
 // version 2
-function promApi2(result) {
-  console.log('result: ', result)
+function promApi2(resultNum, result) {
+  console.log(`result${resultNum}: `, result)
 
   return new Promise(function(res, rej) {
     res()
   }) 
 }
 
-promApi2('1').then(promApi2('2')).then(promApi2('3'))
+promApi2('', '1').then(promApi2('2', '2')).then(promApi2('3', '3'))
